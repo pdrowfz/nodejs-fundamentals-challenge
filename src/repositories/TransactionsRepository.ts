f5b2363c67be9bd4ffc71f271f47303e97a2fc95
Pedro Zanetti <pdrowfz@gmail.com>
2020-04-23 00:46:20 -0300

    Solving the Node.js fundamentals' challenge.

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,34 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const incomes = this.transactions.filter(
+      transaction => transaction.type === 'income',
+    );
+    const outcomes = this.transactions.filter(
+      transaction => transaction.type === 'outcome',
+    );
+    const valuesReducer = (
+      valuesSum: number,
+      currentTransaction: Transaction,
+    ): number => valuesSum + currentTransaction.value;
+
+    const income = incomes.reduce(valuesReducer, 0);
+    const outcome = outcomes.reduce(valuesReducer, 0);
+    const total = income - outcome;
+
+    return { income, outcome, total };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
